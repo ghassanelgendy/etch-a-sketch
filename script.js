33d5ad0f5dab93bfa8edcf3ef5348092501f4411
Ghassan <ghassanelgendyy@gmail.com>
2023-07-11 23:28:48 +0300

    Add the main functions to run

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-let pixels = 400;
-function generateGrid(pixels, canvasSize = 500) {
-	const canvas = document.getElementById("canvas");
+let pixels = 100;
+const canvas = document.getElementById("canvas");
+const canvasSize = canvas.style.width.slice(0, -2);
+
+generateGrid(pixels, canvasSize);
+function generateGrid(pixels, canvasSize = 300) {
 	for (let i = 0; i < pixels; i++) {
 		let pixel = document.createElement("div");
 		pixel.classList.add("pixel");
@@ -11,4 +14,33 @@ function generateGrid(pixels, canvasSize = 500) {
 	}
 }
 
-generateGrid(pixels);
+const grid = document.querySelectorAll(".pixel");
+Array.from(grid);
+
+function colorGrid(e) {
+	canvas.addEventListener(["mousedown" || "touchstart"], colorGrid, {
+		capture: false,
+		passive: true,
+	});
+
+	canvas.addEventListener(["mousemove" || "touchmove"], colorGrid, {
+		capture: false,
+		passive: true,
+	});
+
+	for (let i = 0; i < grid.length; i++) {
+		if (
+			e.clientX <= grid[i].getBoundingClientRect().right &&
+			e.clientX >= grid[i].getBoundingClientRect().left &&
+			e.clientY <= grid[i].getBoundingClientRect().bottom &&
+			e.clientY >= grid[i].getBoundingClientRect().top &&
+			e.buttons > 0
+		) {
+			grid[i].classList.add("colored");
+		}
+	}
+}
+canvas.addEventListener(["mousedown" || "touchstart"], colorGrid, {
+	capture: false,
+	passive: true,
+});
