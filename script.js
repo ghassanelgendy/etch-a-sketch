8c6cd850ad803876e42df2048561debf475c8b2c
Ghassan <ghassanelgendyy@gmail.com>
2023-07-12 18:38:38 +0300

    Bug Fix Add New Features
    Fixed the bug in generating the grid
    Added a fade out effect after clicking starting button

--- script.js
+++ script.js
@@ -1,60 +1,90 @@
 const canvas = document.getElementById("canvas");
 const canvasSize = 300;
 const startButton = document.getElementById("startButton");
-let pixels;
-startButton.addEventListener("click", () => {
-	const welcomeScreen = document.getElementById("welcomeScreen");
-	const pixelGet = document.getElementsByName("canvasSize");
+const welcomeScreen = document.getElementById("welcomeScreen");
+const pixelGet = document.getElementsByName("canvasSize");
 
+//returns the pixels chosen by user
+function setCanvasSize() {
 	for (let i = 0; i < pixelGet.length; i++) {
 		if (pixelGet[i].checked) {
 			pixels = pixelGet[i].value;
 		}
 	}
+	console.log("function setCanvas:", pixels);
+	return pixels;
+}
 
-	generateGrid((pixels = 100), canvasSize);
-
-	function generateGrid(pixels, canvasSize) {
-		for (let i = 0; i < pixels; i++) {
-			let pixel = document.createElement("div");
-			pixel.classList.add("pixel");
-			const pixelSize = canvasSize / Math.sqrt(pixels);
-			pixel.style.width = `${pixelSize}px`;
-			pixel.style.height = `${pixelSize}px`;
-			canvas.appendChild(pixel);
-		}
+//generates the grid
+function generateGrid(pixels = 16, canvasSize) {
+	for (let i = 0; i < pixels; i++) {
+		let pixel = document.createElement("div");
+		pixel.classList.add("pixel");
+		const pixelSize = canvasSize / Math.sqrt(pixels);
+		pixel.style.width = `${pixelSize}px`;
+		pixel.style.height = `${pixelSize}px`;
+		canvas.appendChild(pixel);
 	}
+	console.log("function generateGrid:", pixels);
+}
 
-	const grid = document.querySelectorAll(".pixel");
-	Array.from(grid);
+//removes the welcome screen
+function fadeOut(el) {
+	el.parentElement.classList.add("faded-out");
+	el.style.display = "none";
+}
+
+//set  the pixels chosen by user and runs the generate grid function
+startButton.addEventListener(
+	"click",
+	() => {
+		const pixels = setCanvasSize();
+		generateGrid(pixels, canvasSize);
+	},
+	{ once: true }
+);
+//if the canvas is empty and the welcome screen is not displayed, generate a grid
+if (!canvas.hasChildNodes() && welcomeScreen.style.display === "none") {
+	generateGrid(16, canvasSize);
+}
+//fade out the welcome screen
+startButton.addEventListener(
+	"click",
+	(e) => {
+		fadeOut(e.target);
+	},
+	{
+		once: true,
+	}
+);
+
+const grid = document.querySelectorAll(".pixel");
+Array.from(grid);
+canvas.addEventListener("mousedown", colorGrid, {
+	capture: false,
+	passive: true,
+});
+
+function colorGrid(e) {
 	canvas.addEventListener("mousedown", colorGrid, {
 		capture: false,
 		passive: true,
 	});
 
-	function colorGrid(e) {
-		canvas.addEventListener("mousedown", colorGrid, {
-			capture: false,
-			passive: true,
-		});
+	canvas.addEventListener("mousemove", colorGrid, {
+		capture: false,
+		passive: true,
+	});
 
-		canvas.addEventListener("mousemove", colorGrid, {
-			capture: false,
-			passive: true,
-		});
-
-		for (let i = 0; i < grid.length; i++) {
-			if (
-				e.clientX <= grid[i].getBoundingClientRect().right &&
-				e.clientX >= grid[i].getBoundingClientRect().left &&
-				e.clientY <= grid[i].getBoundingClientRect().bottom &&
-				e.clientY >= grid[i].getBoundingClientRect().top &&
-				e.buttons > 0
-			) {
-				grid[i].classList.add("colored");
-			}
+	for (let i = 0; i < grid.length; i++) {
+		if (
+			e.clientX <= grid[i].getBoundingClientRect().right &&
+			e.clientX >= grid[i].getBoundingClientRect().left &&
+			e.clientY <= grid[i].getBoundingClientRect().bottom &&
+			e.clientY >= grid[i].getBoundingClientRect().top &&
+			e.buttons > 0
+		) {
+			grid[i].classList.add("colored");
 		}
 	}
-
-	welcomeScreen.style.display = "none";
-});
+}
