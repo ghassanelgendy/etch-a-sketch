d29ea84017ec7547bee11fe02aad06a0843cb5c4
Ghassan <ghassanelgendyy@gmail.com>
2023-07-12 05:03:22 +0300

    Fixed Bug Brush Not Working

--- script.js
+++ script.js
@@ -11,48 +11,50 @@ startButton.addEventListener("click", () => {
 			pixels = pixelGet[i].value;
 		}
 	}
-	generateGrid(pixels, canvasSize);
 
-	welcomeScreen.style.display = "none";
-});
-function generateGrid(pixels, canvasSize) {
-	for (let i = 0; i < pixels; i++) {
-		let pixel = document.createElement("div");
-		pixel.classList.add("pixel");
-		const pixelSize = canvasSize / Math.sqrt(pixels);
-		pixel.style.width = `${pixelSize}px`;
-		pixel.style.height = `${pixelSize}px`;
-		canvas.appendChild(pixel);
+	generateGrid((pixels = 100), canvasSize);
+
+	function generateGrid(pixels, canvasSize) {
+		for (let i = 0; i < pixels; i++) {
+			let pixel = document.createElement("div");
+			pixel.classList.add("pixel");
+			const pixelSize = canvasSize / Math.sqrt(pixels);
+			pixel.style.width = `${pixelSize}px`;
+			pixel.style.height = `${pixelSize}px`;
+			canvas.appendChild(pixel);
+		}
 	}
-}
 
-const grid = document.querySelectorAll(".pixel");
-Array.from(grid);
-
-function colorGrid(e) {
+	const grid = document.querySelectorAll(".pixel");
+	Array.from(grid);
 	canvas.addEventListener("mousedown", colorGrid, {
 		capture: false,
 		passive: true,
 	});
 
-	canvas.addEventListener("mousemove", colorGrid, {
-		capture: false,
-		passive: true,
-	});
+	function colorGrid(e) {
+		canvas.addEventListener("mousedown", colorGrid, {
+			capture: false,
+			passive: true,
+		});
 
-	for (let i = 0; i < grid.length; i++) {
-		if (
-			e.clientX <= grid[i].getBoundingClientRect().right &&
-			e.clientX >= grid[i].getBoundingClientRect().left &&
-			e.clientY <= grid[i].getBoundingClientRect().bottom &&
-			e.clientY >= grid[i].getBoundingClientRect().top &&
-			e.buttons > 0
-		) {
-			grid[i].classList.add("colored");
+		canvas.addEventListener("mousemove", colorGrid, {
+			capture: false,
+			passive: true,
+		});
+
+		for (let i = 0; i < grid.length; i++) {
+			if (
+				e.clientX <= grid[i].getBoundingClientRect().right &&
+				e.clientX >= grid[i].getBoundingClientRect().left &&
+				e.clientY <= grid[i].getBoundingClientRect().bottom &&
+				e.clientY >= grid[i].getBoundingClientRect().top &&
+				e.buttons > 0
+			) {
+				grid[i].classList.add("colored");
+			}
 		}
 	}
-}
-canvas.addEventListener("mousedown", colorGrid, {
-	capture: false,
-	passive: true,
+
+	welcomeScreen.style.display = "none";
 });
